a7a0ca8ad8802835662628c1d8cd71eeb165db11
Kylian Dupouy Peron <kdperon@yahoo.com>
2025-01-15 09:04:59 +0100

    update

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-16 px-5 flex items-center">
+    <header className="h-16 px-5 flex items-center border-b-2">
       <h1 className="text-2xl">Tower defense</h1>
     </header>
   );
